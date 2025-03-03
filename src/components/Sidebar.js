6c2b36584288a5509a0446884e33413ff34f46f2
Navaporn Intharuck <mintharuck@gmail.com>
2025-03-02 18:17:46 -0600

    sidebar adjustments

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sidebar = () => {
+  const [width, setWidth] = useState(200); // Initial width of 200px
+  const [isResizing, setIsResizing] = useState(false);
+
+  const handleMouseDown = () => {
+    setIsResizing(true);
+  };
+
+  const handleMouseMove = (e) => {
+    if (isResizing) {
+      setWidth(e.clientX);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsResizing(false);
+  };
+
   return (
-    <aside className="sidebar">
+    <div
+      className="sidebar"
+      style={{ width: `${width}px` }}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
+      <div className="resizer" onMouseDown={handleMouseDown}></div>
       <nav>
         <ul>
           <li><a href="#home">Home</a></li>
@@ -10,7 +33,7 @@ const Sidebar = () => {
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
-    </aside>
+    </div>
   );
 };
 
