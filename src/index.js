4c096710d18d1c8d75c17513cc8aeb48dbba2d79
Navaporn Intharuck <mintharuck@gmail.com>
2025-04-22 00:20:43 -0500

    update render functino

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Use ReactDOM from 'react-dom/client' for React 18
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,12 +15,15 @@ const addAdSenseScript = () => {
 
 // Call the function to add the AdSense script
 addAdSenseScript();
-ReactDOM.render(
+
+// Create the root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
