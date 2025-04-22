4b91ae06ac81ecaddcb27bb5507fba974d2bc6a6
Navaporn Intharuck <mintharuck@gmail.com>
2025-04-22 00:02:55 -0500

    add adds to index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Dynamically add the AdSense script
+const addAdSenseScript = () => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2470775733308737';
+  script.crossOrigin = 'anonymous';
+  document.head.appendChild(script);
+};
+
+// Call the function to add the AdSense script
+addAdSenseScript();
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
